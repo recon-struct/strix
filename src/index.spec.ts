0f15b015a0b433ff722a7a2cad4d76d0806f42d5
Hans Oksendahl <hansoksendahl@gmail.com>
2024-05-11 20:58:34 -0700

    inital commit

---
+++ src/index.spec.ts
@@ -0,0 +1,78 @@
+import { describe, expect, it } from 'bun:test'
+import createT from './index'
+
+describe('createT', () => {
+  it('It should kick ass and take names', () => {
+    const expected = 'Kick ass and take names.'
+    const t = createT({
+      motto: 'Kick {{this}} and take {{that}}.',
+    } as const)
+    const actual = t('motto', { this: 'ass', that: 'names' } as const)
+
+    expect(actual).toBe(expected)
+  })
+
+  it('It should allow for setting different capture settings', () => {
+    const expected = 'We are the champions!'
+    const t = createT(
+      {
+        anthem: 'We are the ::title::!',
+      } as const,
+      { start: '::', end: '::' } as const,
+    )
+    const actual = t('anthem', { title: 'champions' } as const)
+
+    expect(actual).toBe(expected)
+  })
+
+  it('It should support templates that contain no variables', () => {
+    const expected = 'I am a template string'
+    const t = createT({
+      template: 'I am a template string',
+    } as const)
+    const actual = t('template')
+    expect(actual).toBe(expected)
+  })
+
+  it('should support nesting', () => {
+    const aa = 'aa'
+    const ab = 'ab'
+    const ac = 'ac'
+    const ba = 'ba'
+    const bb = 'bb'
+    const bc = 'bc'
+    const ca = 'ca'
+    const cb = 'cb'
+    const cc = 'cc'
+
+    const t = createT({
+      a: {
+        a: 'aa',
+        b: 'ab',
+        c: 'ac',
+      },
+      b: {
+        a: 'ba',
+        b: 'bb',
+        c: 'bc',
+      },
+      c: {
+        a: 'ca',
+        b: 'cb',
+        c: 'cc',
+      },
+    } as const)
+
+    expect(t('a.a')).toBe(aa)
+    expect(t('a.b')).toBe(ab)
+    expect(t('a.c')).toBe(ac)
+    expect(t('b.a')).toBe(ba)
+    expect(t('b.b')).toBe(bb)
+    expect(t('b.c')).toBe(bc)
+    expect(t('c.a')).toBe(ca)
+    expect(t('c.b')).toBe(cb)
+    expect(t('c.c')).toBe(cc)
+  })
+})
+
+export {}
---
+++ src/index.ts
@@ -0,0 +1,101 @@
+import type { AnyObject } from '@recon-struct/utility-types/dist/any/any-object'
+import type { AnyPrimitive } from '@recon-struct/utility-types/dist/any/any-primitive'
+import type {
+  DeepObject,
+  GetKeys,
+} from '@recon-struct/utility-types/dist/object/get-keys'
+import type { GetValue } from '@recon-struct/utility-types/dist/object/get-value'
+import type { CaptureGroup } from '@recon-struct/utility-types/dist/string/utils'
+import { getDeepProp, interpolate } from '@recon-struct/utils'
+
+export type DefaultCaputreGroup = CaptureGroup<'{{', '}}'>
+
+/**
+ * Extracts captured groups from a string based on a specified capture group pattern.
+ *
+ * @typeParam A - The input string type.
+ * @typeParam B - The capture group pattern type.
+ * @typeParam C - The accumulated captured groups type.
+ * @param {A} input - The input string.
+ * @returns {C} - The accumulated captured groups.
+ */
+export type Capture<
+  A extends DeepObject | AnyPrimitive,
+  B extends CaptureGroup = DefaultCaputreGroup,
+  C extends string = never,
+> = A extends `${string}${B['start']}${infer D}${B['end']}${infer E}`
+  ? Capture<E, B, C | D>
+  : C
+
+/**
+ * Represents an interpolation type.
+ *
+ * @typeParam A - The type of the object being interpolated.
+ * @typeParam B - The type of the capture group.
+ * @typeParam C - The type of the object with captured values.
+ * @typeParam D - The type of the object being interpolated (default: A).
+ * @typeParam E - The type of the captured value.
+ */
+export type Interpolation<
+  A extends DeepObject | AnyPrimitive,
+  B extends CaptureGroup = DefaultCaputreGroup,
+  C extends AnyObject<Capture<A, B>, string> = AnyObject<Capture<A, B>, string>,
+  D extends DeepObject | AnyPrimitive = A,
+  E extends Capture<A, B> = Capture<A, B>,
+> = D extends `${infer F}${B['start']}${infer G}${B['end']}${infer H}`
+  ? G extends E
+    ? Interpolation<A, B, C, `${F}${C[G]}${H}`, E>
+    : never
+  : D
+
+const DEFAULT_CAPTURE_GROUP: DefaultCaputreGroup = {
+  start: '{{',
+  end: '}}',
+}
+
+/**
+ * Creates a template function that allows for dynamic interpolation of
+ * templates.
+ *
+ * @typeParam A - The type of the templates object.
+ * @typeParam B - The type of the capture group.
+ * @param {A} templates - The object containing the templates.
+ * @param {B} captureGroup - The capture group to use for interpolation.
+ *        Defaults to DEFAULT_CAPTURE_GROUP.
+ * @returns {templateFunction} - The template function.
+ */
+const createT = <
+  A extends DeepObject,
+  B extends CaptureGroup = DefaultCaputreGroup,
+>(
+  templates: A,
+  captureGroup: B = DEFAULT_CAPTURE_GROUP as B,
+) => {
+  /**
+   * Return a curried function that takes a `key` defined in `templates` and
+   * an object containing the variables defined for that function.
+   * @param key
+   * @param variables
+   * @returns
+   */
+  const templateFunction = <
+    C extends GetKeys<A>,
+    D extends AnyObject<Capture<GetValue<A, C>, B>, string>,
+  >(
+    key: C,
+    ...params: Capture<GetValue<A, C>, B> extends never ? [] : [D]
+  ): Interpolation<GetValue<A, C>, B, D> => {
+    const variables = params[0] ?? {}
+    const template = getDeepProp(templates, key)
+
+    return interpolate(
+      template as string,
+      captureGroup,
+      variables,
+    ) as Interpolation<GetValue<A, C>, B, D>
+  }
+
+  return templateFunction
+}
+
+export default createT
